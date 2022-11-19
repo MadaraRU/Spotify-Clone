ce615719d0e4cf4c0ce683eac4fd33f2033a0d66
MadaraRU <78321649+MadaraRU@users.noreply.github.com>
2022-11-19 13:53:43 +0100

    implementing the Discover component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
+import {
+  ArtistDetails,
+  TopArtists,
+  AroundYou,
+  Discover,
+  Search,
+  SongDetails,
+  TopCharts,
+} from './pages';
 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
@@ -10,7 +18,8 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#861237]">
+        {/* <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]"> */}
         <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
